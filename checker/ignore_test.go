35bbd8a0ee521d91aa1d3f3b65603c17cee066e1
Anton Tolokan <derbylock@github.com>
2023-06-01 12:11:32 +0300

    Fixed ignorance for errors with the same error message and prefixed paths

--- checker/ignore_test.go
+++ checker/ignore_test.go
@@ -21,3 +21,29 @@ func TestIgnore(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, 5, len(errs))
 }
+
+func TestIgnorePrefixedErrs(t *testing.T) {
+	errs := []checker.BackwardCompatibilityError{
+		{
+			Id:        "err1",
+			Text:      "err1-text",
+			Comment:   "",
+			Level:     checker.ERR,
+			Operation: "GET",
+			Path:      "/objects",
+			Source:    "",
+		},
+		{
+			Id:        "err1",
+			Text:      "err1-text",
+			Comment:   "",
+			Level:     checker.ERR,
+			Operation: "GET",
+			Path:      "/objects/{object_id}",
+			Source:    "",
+		},
+	}
+	errs, err := checker.ProcessIgnoredBackwardCompatibilityErrors(checker.ERR, errs, "../data/ignore-err-prefixes.txt")
+	require.NoError(t, err)
+	require.Equal(t, 0, len(errs))
+}
